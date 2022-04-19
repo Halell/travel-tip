69e0a36f770e2eab451720fe1b1e2ffcb1213c5a
Halell <halellhofman@gmail.com>
2022-04-19 14:24:51 +0300

    test2

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -4,16 +4,16 @@ export const locService = {
 
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
 
 function getLocs() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(locs);
+            resolve(locs)
         }, 2000)
-    });
+    })
 }
 
-
+console.log('hay')
